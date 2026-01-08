019855e29931b5f6650396b2da537823edfc16ef
Mateus Facioli <mateus_rf@hotmail.com>
2026-01-08 14:23:01 -0300

    Initial Commit

---
+++ testeMVVM/testeMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeMVVMApp.swift
+//  testeMVVM
+//
+//  Created by Mateus Rodrigues on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct testeMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testeMVVMTests/testeMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  testeMVVMTests.swift
+//  testeMVVMTests
+//
+//  Created by Mateus Rodrigues on 08/01/26.
+//
+
+import Testing
+@testable import testeMVVM
+
+struct testeMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
